1aca359c216935da1d01c75af0f7c3660a19a986
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-12-21 16:54:13 +0530

    Added components for coin detail screen

---
+++ app/src/main/java/com/arun/cryptocurrencyapp/presentation/coin_detail/components/CoinTag.kt
@@ -0,0 +1,31 @@
+package com.arun.cryptocurrencyapp.presentation.coin_detail.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.arun.cryptocurrencyapp.data.remote.dto.Tag
+
+@Composable
+fun CoinTag(tag: String){
+    Box(modifier = Modifier
+        .border(
+            1.dp,
+            color = MaterialTheme.colors.primary,
+            shape = RoundedCornerShape(100.dp)
+        )
+        .padding(10.dp)
+    ){
+        Text(text = tag,
+            color = MaterialTheme.colors.primary,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.body2
+            )
+    }
+}
